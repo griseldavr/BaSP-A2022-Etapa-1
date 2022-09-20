679cc515e05d0c2f414758471bcb85269a94810a
Griselda Vera <griseldavr21@gmail.com>
2022-09-20 19:33:18 -0300

    BaSP-W05-2022: Solve exercise 6, functions with javascript

--- Week-05/exercises/06_functions.js
+++ Week-05/exercises/06_functions.js
@@ -4,10 +4,10 @@ console.log ('----------EXERCISE 6 : FUNCTIONS----------');
 resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.*/
 console.log('-----EXERCISE 6.a-----');
 
-function mySum(a, b) {
+function sum(a, b) {
     return a + b;
 }
-var sumFunction = mySum(5,2);
+var sumFunction = sum(5,2);
 console.log ('The sum of a + b = ', sumFunction);
 
 /*A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número;
@@ -15,17 +15,17 @@ de no ser un número, mostrar una alerta aclarando que uno de los parámetros ti
 resultado.*/
 console.log('-----EXERCISE 6.b-----');
 
-function mySum(a,b) {
-    if (typeof a === 'number'&& typeof b === 'number') {
-        return a + b;
-    }
-    else {
+function sum(a,b) {
+    if (typeof a !== 'number'|| typeof b !== 'number') {
         alert ('[ERROR] one of the elements contains an error');
         return NaN;
     }
+    else {
+        return a + b;
+    }
 }
 
-var sumVar = mySum('hello',5);
+var sumVar = sum('hello',5);
 console.log ('The sum of a + b = ', sumVar)
 
 /*Aparte, crear una función validate Integer que reciba un número como parámetro y devuelva verdadero si es un
@@ -44,24 +44,41 @@ números sean enteros. En caso que haya decimales mostrar un alerta con el error
 entero (redondeado).*/
 console.log('-----EXERCISE 6.d-----');
 
-function mySum(a,b) {
-    if (validateInt(a) === true && validateInt(b) === true) {
-        if (typeof a ==='number' && typeof b === 'number') {
-            return a + b;
-        }
-        else {
-            alert ('[ERROR] one of the elements contains an error');
-            return NaN;
-        }
+function invalidInt(a,b) {
+    if (typeof a !=='number'|| typeof b !=='number') {
+        alert ('[ERROR] one of the elements contains an error')
+        return NaN;
     }
-    else  {
+    else if (!validateInt(a)) {
         alert ('[ERROR] one of the element is not an integer');
-        return Math.round(a)&&Math.round(b);
+    return Math.round(a);
     }
+    else if (!validateInt(b)) {
+        alert ('[ERROR] one of the element is not an integer');
+    return Math.round(b);
+    }
+    return a + b;
 }
-var sumVar = mySum(5.5,9.5);
-console.log ('The sum of a + b = ', sumVar)
+
+var sumInt = invalidInt(8,10.45);
+console.log ('The sum of a + b = ', sumInt)
 
 /*Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de la función suma probando
 que todo siga funcionando igual.*/
 console.log('-----EXERCISE 6.e-----');
+
+function newSum(a, b) {
+    var aIsInvalid = invalidInt(a);
+    if (aIsInvalid || Number.isNaN(aIsInvalid)) {
+        return aIsInvalid;
+    }
+
+    var bIsInvalid = invalidInt(b);
+    if (bIsInvalid || Number.isNaN(bIsInvalid)) {
+        return bIsInvalid;
+    }
+    return a + b;
+}
+
+var newSumExecution = sum(10, 'bye');
+console.log("Execute new sum: " , newSumExecution);
